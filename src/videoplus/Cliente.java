e707b4912cc17eda43f56ddbdf058974fca95b34
gugucast <gugucast@espol.edu.ec>
2021-06-11 15:48:57 -0500

    se aggrego clase cliente

---
+++ src/videoplus/Cliente.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package videoplus;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Personal
+ */
+public class Cliente {
+	private ArrayList<Video> listaReproduccion;
+	private String nombre;
+	/**
+	* Constructor que fija el nombre del cliente e inializa listaReproduccion
+	* @param nombre
+	*/
+	public Cliente(String nombre){
+		this.nombre = nombre;
+		//iinicializamos el objeto lista
+                //listaReproduccion = new ArrayList<>;
+                //no se pudo inicializar, daba error
+	}
+		
+	/**
+	* Agregar un video al final de la lista de reproduccion. El cliente no puede tener
+	* mas de 5 videos en la lista. Retorna verdadero si se pudo agregar el video
+	* Retorno falso caso contrario
+	* @param p
+	*/
+	public boolean agregarLista(Video v){
+		//metod size retorna el numero de elementos en el arreglo
+		if(listaReproduccion.size()<5){
+			//el metodo add no abierto al final de la lista
+			listaReproduccion.add(v);
+			return true;
+		}
+		return false;
+	}
+}
+
